b9c20f5cf83ae0729f658d28b52bd431658b884d
Yashi tripathi <yashitripathii28@gmail.com>
2024-04-28 02:37:20 +0530

    added application.yml entries

--- src/main/java/com/learning/quizapp/QuestionController.java
+++ src/main/java/com/learning/quizapp/QuestionController.java
@@ -1,9 +1,15 @@
 package com.learning.quizapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("question") // specifying the path
 public class QuestionController {
-
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+        return "Hi, there are your questions";
+    }
 
 }
